7f2e81361e19b8dc8bc790e6e5a4438a2aa80685
Scaffolder <54202856+amoungui@users.noreply.github.com>
2021-12-14 19:05:06 +0100

    display circular diagram

--- micromobility.py
+++ micromobility.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from pyxlsb import open_workbook as open_xlsb
-
+import matplotlib.pyplot as plt
 
 #modification taille de la page
 st.set_page_config(layout="wide")
@@ -23,14 +23,47 @@ def load_data(nrows):
 	df = pd.DataFrame(df[1:], columns=df[0])	
 	return df.head(nrows) 	
 
+#data = load_data(1000)
+
 def move_mode(df):
-	df = df.loc[:, ['Mode déplacement', 'Nombre de véhicules']]
-	data = df.groupby('Mode déplacement')['Nombre de véhicules'].agg([sum])
-	return data
+	data = df.loc[:, ['Mode déplacement', 'Nombre de véhicules']]
+	data['Mode déplacement'] = data['Mode déplacement'].map({
+														'2 roues motorisées':'2 roues motorisées',
+	                            						'Autobus et autocars':'autobus/autocars',
+	                            						'Trottinettes':'Trottinettes',
+	                            						'Trottinettes + vélos':'Trottinettes',
+	                            						'Véhicule lourds > 3.5t':'Véhicule lourds > 3.5t',
+	                            						'Véhicule légers < 3.5t':'Véhicule légers < 3.5t',
+	                            						'Vélos':'Vélos'},
+	                            						na_action=None)
+	d = data.groupby('Mode déplacement')['Mode déplacement'].value_counts()
+	return d
 
+dt = move_mode(load_data(1000))
+labels = ['2 roues motorisées', 'Trottinettes', 'Vélos', 'autobus/autocars']
 
-st.write(load_data(1000))
+def plotly_charts(df, labels):
+	fig, ax = plt.subplots()
+
+	box = ax.get_position()
+	ax.set_position([box.x0, box.y0, box.width * 1.3, box.height])
+
+	ax.pie(df, labels=labels, explode=(0, 0.02, 0, 0), autopct = "%0.2f%%")
+
+	total = sum(df)
+	plt.legend(
+	    loc='upper left',
+	    labels=['%s, %1.1f%%' % (
+	        l, (float(s) / total) * 100) for l, s in zip(labels, df)],
+	    prop={'size': 12},
+	    bbox_to_anchor=(0.0, 1),
+	    bbox_transform=fig.transFigure
+	)
+
+	return fig
+
 st.write("\n\n")
-
-st.area_chart(move_mode(load_data(1000)))
+st.write(plotly_charts(dt, labels))
+#st.write(dt)
+st.write("\n\n")
 
